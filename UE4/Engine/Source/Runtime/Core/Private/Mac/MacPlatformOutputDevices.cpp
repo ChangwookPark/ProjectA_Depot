bb58c944c42bfd99fa8bfb08e06f82af015b0e9c
ChangwookPark <changwook090@naver.com>
2018-07-28 15:26:25 +0900

    4.19.2 엔진 삭제_5
    분할삭제

--- UE4/Engine/Source/Runtime/Core/Private/Mac/MacPlatformOutputDevices.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "MacPlatformOutputDevices.h"
-
-class FOutputDevice* FMacPlatformOutputDevices::GetEventLog()
-{
-	return NULL;
-}
--- UE4/Engine/Source/Runtime/Slate/Private/Framework/MetaData/DriverMetaData.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "DriverMetaData.h"
-#include "DriverIdMetaData.h"
-
-TSharedRef<ISlateMetaData> FDriverMetaData::Id(FName InTag)
-{
-	return MakeShareable(new FDriverIdMetaData(MoveTemp(InTag)));
-}
