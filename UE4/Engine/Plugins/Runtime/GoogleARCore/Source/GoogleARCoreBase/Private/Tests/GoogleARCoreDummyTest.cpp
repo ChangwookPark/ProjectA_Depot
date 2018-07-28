e8fb4f7ac70a074043f23747d978055bd4bf9aa5
ChangwookPark <changwook090@naver.com>
2018-07-28 15:24:41 +0900

    4.19.2 엔진 삭제_2
    엔진 분할삭제

--- UE4/Engine/Plugins/Runtime/GoogleARCore/Source/GoogleARCoreBase/Private/Tests/GoogleARCoreDummyTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "AutomationTest.h"
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FGoogleARCoreDummyTest, "GoogleARCore.DummyTest", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::EngineFilter)
-
-bool FGoogleARCoreDummyTest::RunTest(const FString& Parameters)
-{
-          return true;
-}
